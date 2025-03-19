c897178a9b7f8b851e09f1021e1d67b25aa0e043
Kalyanlimbu <kalyanlimbu135@gmail.com>
2025-03-19 23:23:03 +0800

    first commit

---
+++ src/main/java/com/polyu/comp3334/secure_storage_system/SecureStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.polyu.comp3334.secure_storage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureStorageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polyu/comp3334/secure_storage_system/SecureStorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polyu.comp3334.secure_storage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureStorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
